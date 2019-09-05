68cab32f2318d1f2cce373a7cc6f2aacaca4d70f
Curry <chenxiaojun@01zhuanche.com>
2019-09-05 11:38:14 +0800

    blueprint, and add threadpool

--- web/web/__init__.py
+++ web/web/__init__.py
@@ -1,9 +1,19 @@
 #-*-coding:utf-8 -*-
 from flask import Flask
 
-app = Flask(__name__)
-app.config["EXPLAIN_TEMPLATE_LOADING"] = True
 
-from .controller import view
+from concurrent.futures import ThreadPoolExecutor
 
-print(__name__)
+# app = Flask(__name__)
+# app.config["EXPLAIN_TEMPLATE_LOADING"] = True
+
+# from .controller import view
+
+# print(__name__)
+
+def create_app():
+    app = Flask(__name__)
+    executor = ThreadPoolExecutor(3)
+    from .controller import view
+    app.register_blueprint(view.bp)
+    return app
--- web/web/controller/view.py
+++ web/web/controller/view.py
@@ -1,18 +1,20 @@
 
 
-from flask import render_template, Flask, request
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
 
-from .. import app
 from ..model.model import getStore, update_store
 
+bp = Blueprint('index', __name__)
 
 
-
-@app.route('/', methods=['GET', 'POST'])
+@bp.route('/', methods=['GET', 'POST'])
 def home():
+    
     return render_template("index.html")
 
-@app.route('/signin', methods=["GET",'POST'])
+@bp.route('/signin', methods=["GET",'POST'])
 def signin():
     # 需要从request对象读取表单内容：
     if request.method == "GET":
@@ -23,12 +25,12 @@ def signin():
             return '<h3>Hello, admin!</h3>  '
         return '<h3>Bad username or password.</h3>'@app.route('/show', methods=['GET'])
 
-@app.route('/show',methods=['GET'])
+@bp.route('/show',methods=['GET'])
 def show():
     store = getStore()
     return render_template("show.html",store=store)   
    
-@app.route('/go_buy',methods=['GET', "POST"])
+@bp.route('/go_buy',methods=['GET', "POST"])
 def go_buy():
     if request.method == "GET":
         fruit = request.args.get("fruit")
